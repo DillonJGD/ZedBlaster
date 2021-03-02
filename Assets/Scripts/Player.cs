5b2d1ce7303ed107d8460a472af6f8a81ba2acaf
Dillon <dillon.jgd@gmail.com>
2021-03-02 11:15:43 -0600

    Created the player bounds for movement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
@@ -14,14 +28,25 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(.05f,0,0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(.95f,0,0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,0.8f,0)).y;
+    }
+
     void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        var deltaY = Input.GetAxis("Vertical");
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        var newXPos = transform.position.x + deltaX * moveSpeed * Time.deltaTime;
-        var newYPos = transform.position.y + deltaY * moveSpeed * Time.deltaTime;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+    
 }
