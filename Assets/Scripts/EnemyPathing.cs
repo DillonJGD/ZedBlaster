514f9d1e202f6b29ef88f2dd2712007b88c6da25
Dillon <dillon.jgd@gmail.com>
2021-03-05 10:04:51 -0600

    Created the initial waypoint system for the enemy pathing.

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    /// Params
+    int waypointIndex = 0;
+    [SerializeField] float moveSpeed = 2f;
+
+    /// Cached References
+    [SerializeField] List<Transform> waypoints;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        MoveAlongPath();
+    }
+
+    void MoveAlongPath()
+    {
+        if(waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(
+                transform.position, 
+                targetPos, 
+                movementThisFrame
+            );
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
